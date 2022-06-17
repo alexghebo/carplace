35aff882c13c8029a8bd27a64d9d3e61f1cdc542
perjuu <106371185+perjuu@users.noreply.github.com>
2022-06-17 19:38:27 +0300

    Revert "Updated register form."

--- src/main/webapp/app/account/register/register.model.ts
+++ src/main/webapp/app/account/register/register.model.ts
@@ -1,12 +1,3 @@
 export class Registration {
-  constructor(
-    public login: string,
-    public email: string,
-    public dealerName: string,
-    public city: string,
-    public address: string,
-    public phone: string,
-    public password: string,
-    public langKey: string
-  ) {}
+  constructor(public login: string, public email: string, public password: string, public langKey: string) {}
 }
