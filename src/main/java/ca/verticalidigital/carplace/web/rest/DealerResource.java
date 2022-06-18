0b0711bb2b5102a6588b3fd4dd88b4bd09434401
allexiulian <fasiealexiulian@gmail.com>
2022-06-19 00:28:26 +0300

    - added new columns to user - added dealer entity

---
+++ src/main/java/ca/verticalidigital/carplace/web/rest/DealerResource.java
@@ -0,0 +1,9 @@
+package ca.verticalidigital.carplace.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dealer")
+public class DealerResource {
+}
