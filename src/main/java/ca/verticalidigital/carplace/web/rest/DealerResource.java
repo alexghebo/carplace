7ce0a40e122e4c8179c13e78f4dd74b33dc26e27
allexiulian <fasiealexiulian@gmail.com>
2022-06-19 00:40:18 +0300

    Revert "- added new columns to user"
    This reverts commit b4b84e82dd6dacf02fb819b4ad78a345350372fb.
    
    -m 'Revert to main branch state'

--- src/main/java/ca/verticalidigital/carplace/web/rest/DealerResource.java
+++
@@ -1,9 +0,0 @@
-package ca.verticalidigital.carplace.web.rest;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/dealer")
-public class DealerResource {
-}
