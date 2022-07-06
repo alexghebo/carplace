40340f9e1ec29a1dbeb6e6871ab7db254f0da23e
SkarlexDev <luciadi771@yahoo.com>
2022-07-06 11:20:32 +0300

    Updated AWSHelper

--- src/main/java/ca/verticalidigital/carplace/importer/AWSHelper.java
+++ src/main/java/ca/verticalidigital/carplace/importer/AWSHelper.java
@@ -1,6 +1,5 @@
 package ca.verticalidigital.carplace.importer;
 
-import ca.verticalidigital.carplace.domain.enumeration.*;
 import ca.verticalidigital.carplace.service.VehicleListingService;
 import ca.verticalidigital.carplace.service.dto.VehicleListingDTO;
 import com.amazonaws.auth.AWSCredentials;
@@ -10,6 +9,9 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import liquibase.repackaged.com.opencsv.exceptions.CsvValidationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -19,16 +21,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 @EnableScheduling
 
 public class AWSHelper {
-    private VehicleListingService vehicleListingService;
+    private final VehicleListingService vehicleListingService;
 
     @Value("${aws.bucket}")
     private String BUCKET_NAME;
@@ -52,43 +51,9 @@ public class AWSHelper {
         AmazonS3 s3 = this.getClient();
         S3Object s3Object = this.getObject(s3);
         BufferedReader br = read(s3Object);
-        List<VehicleListingDTO> list = getListFromCSV(br);
+        CsvSchema schema = createSchema();
+        List<VehicleListingDTO> list = getListFromCSV(br,schema);
         vehicleListingService.saveAll(list);
-    }
-
-    private VehicleListingDTO getDto(String[] data){
-        VehicleListingDTO dto = new VehicleListingDTO();
-        dto.setPrice(Integer.parseInt(data[0]));
-        dto.setYear(Integer.parseInt(data[1]));
-        dto.setMileage(Integer.parseInt(data[2]));
-        dto.setFuel(FuelType.valueOf(data[3].toUpperCase()));
-        dto.setStatus(ListingStatus.valueOf(data[4].toUpperCase()));
-        dto.setInternalNumber(data[5]);
-        dto.setPerformance(Integer.parseInt(data[6]));
-        dto.setVat(valueOfNumber(data[7]));
-        dto.setVin(data[8]);
-        dto.setColour(data[9]);
-        dto.setAc(Ac.valueOf(data[10].toUpperCase()));
-        dto.setEsp(valueOfNumber(data[11]));
-        dto.setAbs(valueOfNumber(data[12]));
-        dto.setDoors(Integer.parseInt(data[13]));
-        dto.setCubicCapacity(Integer.parseInt(data[14]));
-        dto.setNumberOfSeats(Integer.parseInt(data[15]));
-        dto.setEmissionClass(EmissionClass.valueOf(data[16].toUpperCase()));
-        dto.setEmission(Integer.parseInt(data[17]));
-        dto.setGearbox(Gearbox.valueOf(data[18].toUpperCase()));
-        dto.setRegDate(getInstant());
-        return dto;
-    }
-
-    private Boolean valueOfNumber(String number){
-        int nr = Integer.parseInt(number);
-        return nr != 0;
-    }
-
-    private Instant getInstant(){
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        return timestamp.toInstant();
     }
 
     public AmazonS3 getClient(){
@@ -103,19 +68,41 @@ public class AWSHelper {
         return s3.getObject(BUCKET_NAME,OBJECT_NAME);
     }
 
-    private List<VehicleListingDTO> getListFromCSV(BufferedReader br) throws IOException {
-        String line;
-        List<VehicleListingDTO> list = new ArrayList<>();
-        while ((line=br.readLine())!=null){
-            String[] data = line.split(";");
-            list.add(this.getDto(data));
-        }
-        return list;
-    }
-
     public BufferedReader read(S3Object s3Object) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(s3Object.getObjectContent()));
         br.readLine();
         return br;
     }
+    private CsvSchema createSchema() {
+        return CsvSchema.builder()
+            .addColumn("price", CsvSchema.ColumnType.NUMBER)
+            .addColumn("year", CsvSchema.ColumnType.NUMBER)
+            .addColumn("mileage", CsvSchema.ColumnType.NUMBER)
+            .addColumn("fuel", CsvSchema.ColumnType.STRING)
+            .addColumn("status", CsvSchema.ColumnType.STRING)
+            .addColumn("internalNumber", CsvSchema.ColumnType.STRING)
+            .addColumn("performance", CsvSchema.ColumnType.STRING)
+            .addColumn("vat", CsvSchema.ColumnType.BOOLEAN)
+            .addColumn("vin", CsvSchema.ColumnType.STRING)
+            .addColumn("colour", CsvSchema.ColumnType.STRING)
+            .addColumn("ac", CsvSchema.ColumnType.STRING)
+            .addColumn("esp", CsvSchema.ColumnType.BOOLEAN)
+            .addColumn("abs", CsvSchema.ColumnType.BOOLEAN)
+            .addColumn("doors", CsvSchema.ColumnType.NUMBER)
+            .addColumn("cubicCapacity", CsvSchema.ColumnType.NUMBER)
+            .addColumn("numberOfSeats", CsvSchema.ColumnType.NUMBER)
+            .addColumn("emissionClass", CsvSchema.ColumnType.STRING)
+            .addColumn("emission", CsvSchema.ColumnType.NUMBER)
+            .addColumn("gearbox", CsvSchema.ColumnType.STRING)
+            .build()
+            .withColumnSeparator(';');
+    }
+    private List<VehicleListingDTO> getListFromCSV(BufferedReader br, CsvSchema schema) throws IOException {
+        MappingIterator<VehicleListingDTO> list
+            = new CsvMapper().readerWithTypedSchemaFor(VehicleListingDTO.class)
+            .with(schema)
+            .readValues(br);
+        return list.readAll();
+    }
+
 }
