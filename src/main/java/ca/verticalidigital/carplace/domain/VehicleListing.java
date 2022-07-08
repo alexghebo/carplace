6e3d8c3bf2ed9dd544edf42f11bf64a71a24a239
SkarlexDev <luciadi771@yahoo.com>
2022-07-08 10:33:20 +0300

    Updated mail

--- src/main/java/ca/verticalidigital/carplace/domain/VehicleListing.java
+++ src/main/java/ca/verticalidigital/carplace/domain/VehicleListing.java
@@ -1,5 +1,6 @@
 package ca.verticalidigital.carplace.domain;
 
+import ca.verticalidigital.carplace.config.Constants;
 import ca.verticalidigital.carplace.domain.enumeration.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Cache;
@@ -7,6 +8,7 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.time.Instant;
@@ -47,6 +49,7 @@ public class VehicleListing implements Serializable {
 
     @NotNull
     @Size(max = 40)
+    @Pattern(regexp= Constants.INTERNAL_REGEX)
     @Column(name="internal_number", length = 40)
     private String internalNumber;
 
